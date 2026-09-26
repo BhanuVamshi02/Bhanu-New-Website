c2339b92c4c643cbef7d8662b61d883b9ea460b0
Bhanu Vamshi Anumula <121792167+BhanuVamshi02@users.noreply.github.com>
2026-09-26 22:05:41 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -183,7 +183,7 @@ function initTrailer() {
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen style="width:100%,height:100%">
+            allowfullscreen style="width:100%;height:100%;">
           </iframe>
         </div>
       `;
